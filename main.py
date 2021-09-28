582b83cd6fcbc067c2912ff4ae7941e1851f5223
umar2303 <umar.abdymalikov@gmail.com>
2021-09-28 20:22:07 +0600

    Test  commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# список книг
+# список читателей {имя: {Назв.книги: автор}}
+
+# функция, которая выводит информацию о книгах
+#  которые есть и о читателях
+
+#  функция, которая регистрирует читателя и выдает книгу
+
+#  функция, которая возвращает книгу
+
+#  функция менеджер, которая распределяет вызов других функций
+
+# books = {'Financial': 'Theodor Draiser', 
+#   'Rich dad, poor dad': 'Robert Kiyosaki',
+#   'Think and get rich': 'Napoleon Hill'}
+
+# readers = {'Alex': {'Financial': 'Theodor Draiser'}}
+
+# def info():
+#     print(f'На данный момент список читателей такой: {readers}')
+#     print(f'Список книг такой: {books}')
+
+# def register():
+#     print(f'Какую книгу вы хотите получить? Выберите из {books}')
+#     book = input('Введите название книги: ')
+#     name = input('Введите свое имя: ')
+#     readers[name] = {book: books[book]}
+#     print(f'Ваша заявка успешно оформлена! {readers}')
+
+# def 
+
+
+Hello, world!
