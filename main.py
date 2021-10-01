93b0b35c4bd3b80e52ec3e0bc34e7b1703fd567c
umar2303 <91546998+umar2303@users.noreply.github.com>
2021-10-01 19:37:49 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
-# список книг
-# список читателей {имя: {Назв.книги: автор}}
+# JSON(javascript object notation)
+# универсальный формат для передачи данных
 
-# функция, которая выводит информацию о книгах
-#  которые есть и о читателях
+# import json
+# data = {'name': 'Tom', 'age': 30, 'gender': 'male'}
+# json_obj = json.dumps(data)
+# # print(type(json_obj)
+# # print(json_obj)
+# python_obj = json.loads(json_obj)
+# print(type(python_obj))
 
-#  функция, которая регистрирует читателя и выдает книгу
+# Python         JSON
+# dict           Object
+# list           Array(массив)
+# set            -
+# str            String
+# int            Number  
+# float          Nmber
+# True           true
+# False          false
+# None           null
+# tuple          Array
 
-#  функция, которая возвращает книгу
+# import json
+# dict1 = {'name': 'Tom', 'age': 30, 'gender': 'male'}
+# with open('data.json', 'a+') as f:
+#      json.dump(dict1, f)
 
-#  функция менеджер, которая распределяет вызов других функций
-
-# books = {'Financial': 'Theodor Draiser', 
-#   'Rich dad, poor dad': 'Robert Kiyosaki',
-#   'Think and get rich': 'Napoleon Hill'}
-
-# readers = {'Alex': {'Financial': 'Theodor Draiser'}}
-
-# def info():
-#     print(f'На данный момент список читателей такой: {readers}')
-#     print(f'Список книг такой: {books}')
-
-# def register():
-#     print(f'Какую книгу вы хотите получить? Выберите из {books}')
-#     book = input('Введите название книги: ')
-#     name = input('Введите свое имя: ')
-#     readers[name] = {book: books[book]}
-#     print(f'Ваша заявка успешно оформлена! {readers}')
-
-# def 
+# import json
+# with open('data.json', 'r+') as f:
+#     python_obj = json.loads(f.read())
+#     print(python_obj)
+#     print(f)
 
 
-Hello, world!
+
+
+
+
+
+
+
